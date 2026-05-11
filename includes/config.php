f174ca2d347a956ecf152cccaaa613cfba9003b8
Coapdaawatulrusul <callingoftheallprophetsmw@gmail.com>
2026-05-11 12:31:01 +0200

    Create includes/config.php

---
+++ includes/config.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+$host = 'localhost';
+$dbuser = 'root';
+$dbpass = '';
+$dbname = 'chig_tech';
+$conn = new mysqli($host, $dbuser, $dbpass, $dbname);
+if ($conn->connect_error) die("Connection failed: " . $conn->connect_error);
+?>
